d9955b42ac57ad16a97714ee3ccae76f9a4979dc
vincent youmssi <vincentyoumssi@gmail.com>
2024-06-15 12:01:46 +0100

    add of weather app project to repository

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../search";
 
 export default function Weather() {
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
-  async function fetchWeatherData() {
+  async function fetchWeatherData(param) {
+    setLoading(true);
     try {
-      const response = await fetch();
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=e34b4c51d8c2b7bf48d5217fe52ff79e`
+      );
+      const data = await response.json();
+      console.log(data, "data");
+      if (data) {
+        setWeatherData(data);
+        setLoading(false);
+      }
     } catch (e) {
+      setLoading(false);
       console.log(e);
     }
   }
+  const getCurrentDate = () => {
+    return new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+  };
+  useEffect(() => {
+    fetchWeatherData("yaounde");
+  }, []);
 
-  async function handleSearch() {}
+  function handleSearch() {
+    fetchWeatherData(search);
+  }
+  console.log(weatherData);
   return (
     <section className="weather container">
       <Search
@@ -21,7 +45,33 @@ export default function Weather() {
         setSearch={setSearch}
         handleSearch={handleSearch}
       />
-      weather
+      {loading ? (
+        <h3 className="heading">Loading...</h3>
+      ) : (
+        <div className="content">
+          <h3 className="heading">
+            {weatherData?.name}, <span>{weatherData?.sys.country}</span>
+          </h3>
+          <div className="details">
+            <span className="date">{getCurrentDate()}</span> <br />
+            <span className="temperature">{weatherData?.main?.temp}</span>
+            <p className="description">
+              {weatherData && weatherData.weather && weatherData.weather[0]
+                ? weatherData.weather[0].description
+                : ""}
+            </p>
+            <div className="infos">
+              <span>
+                {weatherData?.wind?.speed} <i>wind Speed</i>
+              </span>
+
+              <span>
+                {weatherData?.main?.humidity}%<i> Humidity</i>
+              </span>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
